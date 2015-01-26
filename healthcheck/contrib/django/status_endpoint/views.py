e893a860f4a8ad9682f400507948ee20fce1c328
William Stewart <william@synthasite.com>
2015-01-26 13:34:07 +0200

    Fix content_type for JSON responses

--- healthcheck/contrib/django/status_endpoint/views.py
+++ healthcheck/contrib/django/status_endpoint/views.py
@@ -2,10 +2,21 @@ import json
 
 from django.conf import settings
 from django.views.decorators.http import require_http_methods
-from django.http import HttpResponse, HttpResponseServerError
+from django.http import HttpResponse
 
 from healthcheck.healthcheck import (
     DjangoDBsHealthCheck, FilesDontExistHealthCheck, HealthChecker)
+
+
+class JsonResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        kwargs.setdefault('content_type', 'application/json')
+        data = json.dumps(data)
+        super(JsonResponse, self).__init__(content=data, **kwargs)
+
+
+class JsonResponseServerError(JsonResponse):
+    status_code = 500
 
 
 @require_http_methods(['GET'])
@@ -15,16 +26,14 @@ def status(request):
     if getattr(settings, 'STATUS_CHECK_DBS', True):
         checks.append(DjangoDBsHealthCheck())
 
-    files_to_check = getattr(
-        settings, 'STATUS_CHECK_FILES')
+    files_to_check = getattr(settings, 'STATUS_CHECK_FILES')
     if files_to_check:
-        checks.append(
-            FilesDontExistHealthCheck(
-                files_to_check, check_id="quiesce file doesn't exist"))
+        checks.append(FilesDontExistHealthCheck(
+            files_to_check, check_id="quiesce file doesn't exist"))
 
     ok, details = HealthChecker(checks)()
 
     if not ok:
-        return HttpResponseServerError((json.dumps(details)))
+        return JsonResponseServerError(json.dumps(details))
 
-    return HttpResponse(json.dumps(details))
+    return JsonResponse(details)
