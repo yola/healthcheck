a67e3ebf49be588afcc2aff15aad2740437a1abc
Alex Hall <alex@yola.com>
2015-10-29 15:21:43 +0200

    Change how file existence is checked to allow wrong permissions.

--- healthcheck/healthcheck.py
+++ healthcheck/healthcheck.py
@@ -1,5 +1,6 @@
 import abc
-import os.path
+import errno
+import os
 
 
 class HealthCheck(object):
@@ -116,7 +117,7 @@ class FilesExistHealthCheck(ListHealthCheck):
     """Fails if at least one of passed files doesn't exist."""
 
     def check_item(self, filename):
-        file_exists = os.path.isfile(filename)
+        file_exists = _file_exists(filename)
         details = {filename: 'exists' if file_exists else 'NO SUCH FILE'}
         return file_exists, details
 
@@ -125,7 +126,7 @@ class FilesDontExistHealthCheck(ListHealthCheck):
     """Fails if at least one of passed files exists."""
 
     def check_item(self, filename):
-        file_exists = os.path.isfile(filename)
+        file_exists = _file_exists(filename)
         details = {filename: 'FILE EXISTS' if file_exists else 'no such file'}
         return not file_exists, details
 
@@ -181,3 +182,26 @@ class HealthChecker(object):
             return False
 
         return True
+
+
+def _file_exists(path):
+    """Returns True if a file exists at `path` (even if it can't be read),
+    otherwise False.
+
+    This is different from os.path.isfile and os.path.exists which return
+    False if a file exists but the user doesn't have permission to read it."""
+    try:
+        os.stat(path)
+        return True
+    except OSError as e:
+
+        # Permission denied: someone chose the wrong permissions but it exists
+        if e.errno == errno.EACCES:
+            return True
+
+        # File doesn't exist
+        elif e.errno == errno.ENOENT:
+            return False
+
+        # Unkown case
+        raise
