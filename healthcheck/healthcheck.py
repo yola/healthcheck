a82f15cc12122de2d67e4b8807718d4a7754fe79
Alex Hall <alex@yola.com>
2015-10-29 17:10:58 +0200

    Move file_exists function to new utils.py

--- healthcheck/healthcheck.py
+++ healthcheck/healthcheck.py
@@ -1,6 +1,7 @@
 import abc
 import errno
 import os
+import utils
 
 
 class HealthCheck(object):
@@ -117,7 +118,7 @@ class FilesExistHealthCheck(ListHealthCheck):
     """Fails if at least one of passed files doesn't exist."""
 
     def check_item(self, filename):
-        file_exists = _file_exists(filename)
+        file_exists = utils.file_exists(filename)
         details = {filename: 'exists' if file_exists else 'NO SUCH FILE'}
         return file_exists, details
 
@@ -126,7 +127,7 @@ class FilesDontExistHealthCheck(ListHealthCheck):
     """Fails if at least one of passed files exists."""
 
     def check_item(self, filename):
-        file_exists = _file_exists(filename)
+        file_exists = utils.file_exists(filename)
         details = {filename: 'FILE EXISTS' if file_exists else 'no such file'}
         return not file_exists, details
 
@@ -182,27 +183,3 @@ class HealthChecker(object):
             return False
 
         return True
-
-
-def _file_exists(path):
-    """Return True if a file exists at `path` (even if it can't be read),
-    otherwise False.
-
-    This is different from os.path.isfile and os.path.exists which return
-    False if a file exists but the user doesn't have permission to read it.
-    """
-    try:
-        os.stat(path)
-        return True
-    except OSError as e:
-
-        # Permission denied: someone chose the wrong permissions but it exists
-        if e.errno == errno.EACCES:
-            return True
-
-        # File doesn't exist
-        elif e.errno == errno.ENOENT:
-            return False
-
-        # Unknown case
-        raise
---
+++ healthcheck/utils.py
@@ -0,0 +1,26 @@
+import errno
+import os
+
+
+def file_exists(path):
+    """Return True if a file exists at `path` (even if it can't be read),
+    otherwise False.
+
+    This is different from os.path.isfile and os.path.exists which return
+    False if a file exists but the user doesn't have permission to read it.
+    """
+    try:
+        os.stat(path)
+        return True
+    except OSError as e:
+
+        # Permission denied: someone chose the wrong permissions but it exists
+        if e.errno == errno.EACCES:
+            return True
+
+        # File doesn't exist
+        elif e.errno == errno.ENOENT:
+            return False
+
+        # Unknown case
+        raise
