ad8aa74559f0ed0371e16b07b27533ef30b65197
Eugene Koval <eugene@yola.com>
2017-09-06 19:41:09 +0300

    - Removed needless settings.py - Updated requirements.txt

--- healthcheck/contrib/django/status_endpoint/settings.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY = 'None'
-ROOT_URLCONF = 'healthcheck.contrib.django.status_endpoint.urls'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
