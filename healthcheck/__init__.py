d155b8fcad247934e9951c3b83739abd169adeb4
Eugene Koval <eugene@yola.com>
2017-09-07 14:16:56 +0300

    Bump version to avoid conflict at Yolapi

--- healthcheck/__init__.py
+++ healthcheck/__init__.py
@@ -1,5 +1,5 @@
 __doc__ = 'Health Checker for Yola Services'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __url__ = 'https://github.com/yola/healthcheck'
 
 from .checks import (DjangoDBsHealthCheck, FilesDontExistHealthCheck,
